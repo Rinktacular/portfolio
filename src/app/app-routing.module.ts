98d5d422ce5c6e51870695e1fc73917bd5a705c3
Ryan Humphries <ryan.humphries@greenphire.com>
2022-06-16 15:39:22 -0400

    Posts and Post list WIP, want to get the posts looping in .ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+// Angular imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+
+// Application imports
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserPostComponent } from './components/user-post/user-post.component';
+import { UserPostListComponent } from './components/user-post-list/user-post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    UserPostComponent,
+    UserPostListComponent,
   ],
   imports: [
     BrowserModule,
